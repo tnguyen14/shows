76b93e2d0f2d7af8150b66fdce045936e9d890e6
Tri Nguyen <tri@tridnguyen.com>
2020-06-27 01:28:04 -0400

    Sort shows

--- App.svelte
+++ App.svelte
@@ -4,10 +4,20 @@
 
   const showsSSId = '1t9m4G2uujVZhq8VCrw5VQhxCjT9IVXEeV2SspyhB6cU';
 
+  function getShowFromRow(row) {
+    return {
+      title: row.cells[0],
+      rating: row.cells[1]
+    }
+  }
+
   onMount(async () => {
     const resp = await fetch(`${API_URL}/${showsSSId}`).then(r => r.json())
     // first row is the title, ignore it
-    shows.update(n => n.concat(resp.sheets[0].rowData.slice(1)))
+    shows.update(n => n.concat(resp.sheets[0].rowData.slice(1).map(getShowFromRow)).sort((showA, showB) => {
+      // sort from high to low
+      return showB.rating - showA.rating;
+    }))
   });
 </script>
 
@@ -19,8 +29,8 @@
   <tbody>
     {#each $shows as show}
       <tr>
-        <td>{show.cells[0]}</td>
-        <td>{show.cells[1]}</td>
+        <td>{show.title}</td>
+        <td>{show.rating}</td>
       </tr>
     {/each}
   </tbody>
